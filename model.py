33eed5a14a8718769cee5c9367123931df7f7d8d
Varun Suresh <fab.varun@gmail.com>
2017-07-23 16:53:14 -0500

    Added batch norm

--- model.py
+++ model.py
@@ -9,23 +9,23 @@ import torch.nn.functional as F
 class generator(nn.Module):
     def __init__(self, input_size, output_size):
         super(generator, self).__init__()
+        self.conv_tp1 = nn.ConvTranspose2d(100, 512, kernel_size=4, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(512)
         # TODO : Define this in a loop, don't hardcode layer sizes
-        self.conv1 = nn.ConvTranspose2d(100, 512, kernel_size=4, stride=1, bias=False)
-        self.conv2 = nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False)
-        self.conv3 = nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False)
-        self.conv4 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False)
-        self.conv5 = nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False)
+        self.conv_tp2 = nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.conv_tp3 = nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv_tp4 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.conv_tp5 = nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False)
 
     def forward(self, x):
-        # out = self.linear(x)
-        # out = out.view((1, -1, 4, 4))
-        out = self.conv1(x)
-        out = F.relu(out)
-        print out.size()
-        out = F.relu(self.conv2(out))
-        out = F.relu(self.conv3(out))
-        out = F.relu(self.conv4(out))
-        out = F.relu(self.conv5(out))
+        out = F.relu(self.bn1(self.conv_tp1(x)))
+        out = F.relu(self.bn2(self.conv_tp2(out)))
+        out = F.relu(self.bn3(self.conv_tp3(out)))
+        out = F.relu(self.bn4(self.conv_tp4(out)))
+        out = F.relu(self.conv_tp5(out))
         return torch.tanh(out)
 
 
